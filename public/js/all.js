3b3ef049d64434c30a2bcd0c94cdb9a03ece1d92
tantran <octttran@gmail.com>
2016-02-01 13:54:56 -0800

    added update for responsive design for more appealing visualization

--- public/js/all.js
+++ public/js/all.js
@@ -1,5 +1,50 @@
 var app = angular.module('myApp', []);
 
+app.filter('tel', function(){
+  return function (tel) {
+        if (!tel) { return ''; }
+
+        var value = tel.toString().trim().replace(/^\+/, '');
+
+        if (value.match(/[^0-9]/)) {
+            return tel;
+        }
+
+        var country, city, number;
+
+        switch (value.length) {
+            case 10: // +1PPP####### -> C (PPP) ###-####
+                country = 1;
+                city = value.slice(0, 3);
+                number = value.slice(3);
+                break;
+
+            case 11: // +CPPP####### -> CCC (PP) ###-####
+                country = value[0];
+                city = value.slice(1, 4);
+                number = value.slice(4);
+                break;
+
+            case 12: // +CCCPP####### -> CCC (PP) ###-####
+                country = value.slice(0, 3);
+                city = value.slice(3, 5);
+                number = value.slice(5);
+                break;
+
+            default:
+                return tel;
+        }
+
+        if (country == 1) {
+            country = "";
+        }
+
+        number = number.slice(0, 3) + '-' + number.slice(3);
+
+        return (country + " (" + city + ") " + number).trim();
+    };
+});
+
 app.controller('apiController', function($http){
   vm = this;
 
@@ -9,7 +54,8 @@ app.controller('apiController', function($http){
       url: "/info/search",
       data: {
         term: document.getElementById('searchTextKeyword').value,
-        location: document.getElementById('searchTextLocation').value
+        location: document.getElementById('searchTextLocation').value,
+        limit: 9
       }
 
     })
@@ -20,5 +66,19 @@ app.controller('apiController', function($http){
     console.log(data);
     });
 
+  };
+});
+
+app.controller('signUpController', function($scope, $http){
+  // vm = this;
+  $scope.signUp = function(){
+    $http({
+      method: "POST",
+      url: "/signUp",
+      data: {
+        username: this.username,
+        password: this.password
+      }
+    });
   };
 });
